8fc44e8fa063f88c20ffb92cb2736f65a18a9462
qing-wq <2702461713@qq.com>
2023-05-07 17:56:43 +0800

    feat: 登录鉴权，初始化用户信息

---
+++ src/main/java/ink/whi/web/global/GlobalInitHelper.java
@@ -0,0 +1,80 @@
+package ink.whi.web.global;
+
+import ink.whi.api.model.context.ReqInfoContext;
+import ink.whi.api.model.dto.BaseUserInfoDTO;
+import ink.whi.api.model.exception.BusinessException;
+import ink.whi.api.model.exception.StatusEnum;
+import ink.whi.api.util.JwtUtil;
+import ink.whi.service.user.repo.UserDao;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 初始化用户信息
+ * @author: qing
+ * @Date: 2023/4/27
+ */
+@Slf4j
+@Component
+public class GlobalInitHelper {
+
+    private static final String SESSION_KEY = "tan-session";
+
+    @Autowired
+    private UserDao userDao;
+
+    /**
+     * 初始化用户信息
+     * @param reqInfo
+     */
+    public void initUserInfo(ReqInfoContext.ReqInfo reqInfo) {
+        HttpServletRequest request =
+                ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        HttpServletResponse response =
+                ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
+        if (request.getCookies() == null) {
+            return;
+        }
+        for (Cookie cookie : request.getCookies()) {
+            if (SESSION_KEY.equalsIgnoreCase(cookie.getName())) {
+                BaseUserInfoDTO user = VerifyToken(cookie.getValue(), response);
+                if (user != null) {
+                    reqInfo.setUserId(user.getUserId());
+                    reqInfo.setUser(user);
+                }
+            }
+        }
+    }
+
+    /**
+     * 校验token
+     *
+     * @param token
+     * @param response
+     */
+    private BaseUserInfoDTO VerifyToken(String token, HttpServletResponse response) {
+        if (StringUtils.isBlank(token)) {
+            return null;
+        }
+        Long userId = JwtUtil.isVerify(token);
+        BaseUserInfoDTO user = userDao.queryBasicUserInfo(userId);
+        if (user == null) {
+            throw BusinessException.newInstance(StatusEnum.JWT_VERIFY_EXISTS);
+        }
+
+        // 检查token是否需要更新
+        if (JwtUtil.isNeedUpdate(token)) {
+            token = JwtUtil.createToken(userId);
+            response.addCookie(new Cookie(SESSION_KEY, token));
+        }
+        return user;
+    }
+}
---
+++ src/main/java/ink/whi/web/hook/filter/AuthFilter.java
@@ -0,0 +1,120 @@
+package ink.whi.web.hook.filter;
+
+import ink.whi.api.model.context.ReqInfoContext;
+import ink.whi.api.util.CrossUtil;
+import ink.whi.web.global.GlobalInitHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLDecoder;
+
+/**
+ * 鉴权Filter
+ * @author: qing
+ * @Date: 2023/5/6
+ */
+@Slf4j
+@WebFilter(urlPatterns = "/*", filterName = "authFilter", asyncSupported = true)
+public class AuthFilter implements Filter {
+    private static final Logger REQ_LOG = LoggerFactory.getLogger("req");
+
+//    @Autowired
+//    private StatisticsSettingService statisticSettingService;
+
+    @Autowired
+    private GlobalInitHelper globalInitService;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        long start = System.currentTimeMillis();
+        HttpServletRequest req = null;
+        try {
+            req = initReqInfo((HttpServletRequest) request);
+            CrossUtil.buildCors(req, (HttpServletResponse) response);
+            chain.doFilter(req, response);
+        }finally {
+            buildRequestLog(ReqInfoContext.getReqInfo(), req, System.currentTimeMillis() - start);
+            ReqInfoContext.clear();
+        }
+    }
+
+    private HttpServletRequest initReqInfo(HttpServletRequest request) {
+        if (staticURI(request)) {
+            // 静态资源直接放行
+            return request;
+        }
+
+        try {
+            ReqInfoContext.ReqInfo reqInfo = new ReqInfoContext.ReqInfo();
+            reqInfo.setPath(request.getPathInfo());
+            reqInfo.setReferer(request.getHeader("referer"));
+            reqInfo.setUserAgent(request.getHeader("User-Agent"));
+            request = this.wrapperRequest(request, reqInfo);
+            // 校验token
+            globalInitService.initUserInfo(reqInfo);
+            ReqInfoContext.addReqInfo(reqInfo);
+        } catch (Exception e) {
+            log.info("init reqInfo error: " + e.getMessage());
+        }
+        return request;
+    }
+
+    private HttpServletRequest wrapperRequest(HttpServletRequest request, ReqInfoContext.ReqInfo reqInfo) {
+        BodyReaderHttpServletRequestWrapper requestWrapper = new BodyReaderHttpServletRequestWrapper(request);
+        reqInfo.setPayload(requestWrapper.getBodyString());
+        return requestWrapper;
+    }
+
+    /**
+     * 日志输出
+     * @param req
+     * @param request
+     * @param costTime
+     */
+    private void buildRequestLog(ReqInfoContext.ReqInfo req, HttpServletRequest request, long costTime) {
+        if (req == null || staticURI(request)) {
+            return;
+        }
+
+        StringBuilder msg = new StringBuilder();
+        msg.append("method=").append(request.getMethod()).append("; ");
+        if (StringUtils.isNotBlank(req.getReferer())) {
+            msg.append("referer=").append(URLDecoder.decode(req.getReferer())).append("; ");
+        }
+        msg.append("; agent=").append(req.getUserAgent());
+
+        if (req.getUserId() != null) {
+            // 打印用户信息
+            msg.append("; user=").append(req.getUserId());
+        }
+
+        msg.append("; uri=").append(request.getRequestURI());
+        if (StringUtils.isNotBlank(request.getQueryString())) {
+            msg.append('?').append(URLDecoder.decode(request.getQueryString()));
+        }
+
+        msg.append("; payload=").append(req.getPayload());
+        msg.append("; cost=").append(costTime);
+        REQ_LOG.info("{}", msg);
+
+        // todo: 保存请求计数
+//        statisticSettingService.saveRequestCount(req.getClientIp());
+    }
+
+    private boolean staticURI(HttpServletRequest request) {
+        return request == null
+                || request.getRequestURI().endsWith("css")
+                || request.getRequestURI().endsWith("js")
+                || request.getRequestURI().endsWith("png")
+                || request.getRequestURI().endsWith("ico")
+                || request.getRequestURI().endsWith("svg");
+    }
+}
